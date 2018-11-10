63cc37eb2efb337e3051c51126f9b61938d3a10f
Nikita Kozlov <erage@MacBook-Pro-Nikita.local>
2018-11-10 20:11:32 +0300

    add unstable

--- main.go
+++ main.go
@@ -7,27 +7,38 @@ import (
 
 func main() {
 
-	el := initElements()
+	elems := []byte{0x0, 0x1, 0x4}
+	elemss := []byte{0x9, 0x6, 0x3}
+	el := make([][]byte, 0)
+	el = append(el, elems)
+	el = append(el, elemss)
+	//el = append(el, elems)
+	//el = append(el, elems)
+	//el = append(el, elemss)
+	//el = append(el, elemss)
+
+
+	//el := initElements()
 	tree, _ := merkletree.CreateTree(el)
-	//root := merkletree.GetRoot(tree)
-	//println(hex.EncodeToString(root))
-	println(tree)
+	root := merkletree.GetRoot(tree)
+	println(hex.EncodeToString(root))
+	//println(tree)
 }
 
-func initElements() [][]byte {
-	stringArr := make([]string, 0)
-	stringArr = append(stringArr, "4555rr5ffvvf5")
-	stringArr = append(stringArr, "4555rr55")
-	stringArr = append(stringArr, "4555rr55")
-	stringArr = append(stringArr, "4555rr55")
-
-
-	elements := make([][]byte, 0)
-
-	for i := 0; i < len(stringArr); i++ {
-		val, _ := hex.DecodeString(stringArr[i])
-		elements = append(elements, val)
-	}
-
-	return elements
-}
+//func initElements() [][]byte {
+//	stringArr := make([]string, 0)
+//
+//	for o := 0; o < 32; o++ {
+//		stringArr = append(stringArr, string(o)+"t")
+//	}
+//
+//	elements := make([][]byte, 0)
+//
+//	for i := 0; i < len(stringArr); i++ {
+//		val, _ := hex.DecodeString(stringArr[i])
+//		elements = append(elements, val)
+//		println(elements[i])
+//	}
+//
+//	return elements
+//}
--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -2,7 +2,6 @@ package merkletree
 
 import (
 	"crypto/sha256"
-	"encoding/hex"
 )
 
 const (
@@ -24,44 +23,63 @@ func CreateTree(elements [][]byte) ([][][]byte, error) {
 	current := make([][]byte, 0)
 
 	tree := make([][][]byte, 0)
-	for i:=0; i < len(elements); i++ {
-		tree = append(tree, elements)
-	}
-
+	//for i:=0; i < len(elements); i++ {
+	tree = append(tree, elements)
+	//}
 
 	for level := 1; level <= maxLevel; level++ {
 
-		for i := 0; i < len(tree[level - 1]) / 2; i++ {
-
-			left := tree[level - 1][i * 2]
-			right := tree[level - 1][i * 2 + 1]
+		for i := 0; i < len(tree[level-1])/2; i++ {
+			left := tree[level-1][i*2]
+			right := tree[level-1][i*2+1]
 
 			f := sha256.New()
+			bytes := make([]byte, 0)
 
-			if _, err := f.Write(append(left, right...)); err != nil {
+			//_, _ = f.Write(bytes)
+
+			//fmt.Printf("left1 = %x\n", left)
+
+			bytes = append(left, right...)
+
+			//println(left)
+			//fmt.Printf("left2 = %x\n", left)
+
+			if _, err := f.Write(bytes); err != nil {
 				return nil, err
 			}
 
+			//println(hex.EncodeToString(current[level]))
+			//fmt.Printf("%x\n", f.Sum(nil))
+
 			current = append(current, f.Sum(nil))
+
 		}
 
-		if len(current) % 2 == 0 && level < maxLevel {
+
+
+		//current.length % 2 && level < maxLevel
+		if (len(current)%2 > 0) && level < maxLevel {
 			current = append(current, empty)
 		}
 
+		e := make([]byte, 0)
+		e = append(empty, empty...)
+
 		l := sha256.New()
-		_, _ = l.Write(append(empty, empty...))
+		_, _ = l.Write(e)
 		empty = l.Sum(nil)
 
 		tree = append(tree, current)
 
-		println(hex.EncodeToString(current[level]))
+		//println(hex.EncodeToString(current[level]))
 
 	}
 
 	return tree, nil
+
 }
 
-func GetRoot (tree [][][]byte) []byte {
-	return tree[len(tree) - 1][0]
+func GetRoot(tree [][][]byte) []byte {
+	return tree[len(tree)-1][0]
 }
