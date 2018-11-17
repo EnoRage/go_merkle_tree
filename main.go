44971757dd364aaf08dd2bf70d7a3578f616aba4
artemvorobev <artall64@gmail.com>
2018-11-17 03:59:49 +0300

    port js implementation as it is to go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./merkletree"
+	"strings"
 )
 
 func main() {
@@ -16,24 +17,45 @@ func main() {
 	//el = append(el, elemss)
 	//el = append(el, elemss)
 
-	el1 := "1"
-	el2 := "2"
-	el3 := "3"
-	el4 := "4"
+	//el1 := "1"
+	//el2 := "2"
+	//el3 := "3"
+	//el4 := "4"
+	//
+	//elsdf := make([]string, 0)
+	//elsdf = append(elsdf, el1)
+	//elsdf = append(elsdf, el2)
+	//elsdf = append(elsdf, el3)
+	//elsdf = append(elsdf, el4)
 
-	elsdf := make([]string, 0)
-	elsdf = append(elsdf, el1)
-	elsdf = append(elsdf, el2)
-	elsdf = append(elsdf, el3)
-	elsdf = append(elsdf, el4)
+	//MerkleTree
+	//tree := merkletree.CreateTreeTest(elsdf)
+	//tree := merkletree.MerkleTree{}
+	//tree.LoadElements( []string{"1", "2", "3", "4"});
+	//tree.GetLayers()
 
-	merkletree.CreateTreeTest(elsdf)
+	data := []string{"1", "2", "3", "4"}
+	tree := merkletree.NewMerkleTree(data)
 
 	//el := initElements()
 	//tree, _ := merkletree.CreateTree(el)
 	//root := merkletree.GetRoot(tree)
 	//println(hex.EncodeToString(root))
-	//println(tree)
+
+	//println(strings.Join([]string{"uuu","1","2"}, ", "))
+	//println("^")
+
+	for _, level := range tree.Layers {
+		// index is the index where we are
+		//println(level)
+
+		println(strings.Join(level, ", "))
+		//for _, element := range level {
+		//	// index is the index where we are
+		//	fmt.Printf("%s ", element)
+		//}
+	}
+
 }
 
 //func initElements() [][]byte {
--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -1,157 +1,144 @@
 package merkletree
 
-import (
-	"github.com/ethereum/go-ethereum/crypto/sha3"
-	"hash"
-	"strconv"
-)
-
-const (
-	maxLevel = 3
-)
-
-//type Tree struct {
-//}
-//
-//type Data interface {
-//	HashOf() ([]byte, error)
-//}
-
-func initHashFunction() hash.Hash {
-	return sha3.NewKeccak256()
+type MerkleTree struct {
+	Layers [][]string
 }
 
-func CreateTree(elements [][]byte) ([][][]byte, error) {
-	h := initHashFunction()
-	_, _ = h.Write([]byte(string(' ')))
-	empty := h.Sum(nil)
+func NewMerkleTree(data []string) *MerkleTree {
+	tree := new(MerkleTree)
 
-	current := make([][]byte, 0)
-
-	tree := make([][][]byte, 0)
-	//for i:=0; i < len(elements); i++ {
-	tree = append(tree, elements)
-	//}
-
-	for level := 1; level <= maxLevel; level++ {
-
-
-
-		for i := 0; i < len(tree[level-1])/2; i++ {
-			left := tree[level-1][i*2]
-			right := tree[level-1][i*2+1]
-
-			f := initHashFunction()
-			bytes := make([]byte, 0)
-
-			//_, _ = f.Write(bytes)
-
-			//fmt.Printf("left1 = %x\n", left)
-
-			bytes = append(left, right...)
-
-			//println(left)
-			//fmt.Printf("left2 = %x\n", left)
-
-			if _, err := f.Write(bytes); err != nil {
-				return nil, err
-			}
-
-			//println(hex.EncodeToString(current[level]))
-			//fmt.Printf("%x\n", f.Sum(nil))
-
-			current = append(current, f.Sum(nil))
-
-		}
-
-		//current.length % 2 && level < maxLevel
-		if (len(current)%2 > 0) && level < maxLevel {
-			current = append(current, empty)
-		}
-
-		e := make([]byte, 0)
-		e = append(empty, empty...)
-
-		l := initHashFunction()
-		_, _ = l.Write(e)
-		empty = l.Sum(nil)
-
-		tree = append(tree, current)
-
-		//println(hex.EncodeToString(current[level]))
-
-	}
-
-	return tree, nil
-
-}
-
-func GetRoot(tree [][][]byte) []byte {
-	return tree[len(tree)-1][0]
-}
-
-func CreateTreeTest(elements []string) ([][]string) {
-
-	empty := string('f')
-
-	current := make([]string, 0)
-
-
-
-	tree := make([][]string, 0)
-
-	tree = append(tree, elements)
-
-	for level := 1; level <= maxLevel; level++ {
-		println("-----------------------")
-		println("Level " + strconv.Itoa(level))
-
-		for i := 0; i < len(tree[level-1])/2; i++ {
-
-			left := tree[level-1][i*2]
-
-			right := tree[level-1][i*2+1]
-
-			println("left " + left)
-			println("right " + right)
-
-
-			current = append(current, left+right)
-
-			print("current: ")
-			println(current[level-1])
-
-
-		}
-
-		//current.length % 2 && level < maxLevel
-		if (len(current)%2 > 0) && level < maxLevel {
-			current = append(current, empty)
-		}
-
-		e := empty + empty
-
-		empty = e
-
-		tree = append(tree, current)
-		println("-----------------------")
-	}
-
-
-	//println(tree[0][0])
-	//println(tree[0][1])
-	//println(tree[1][0])
-	//println(tree[1][1])
-	//println(tree[2][0])
-	//println(tree[2][1])
-	//println(tree[3][0])
-
-	//println(tree[3][1])
-	//println(tree[4][0])
-	//println(tree[4][1])
-	//println(tree[5][0])
-	//println(tree[5][1])
+	lambda := func(x string) string { return hashFunction(x) }
+	tree.Layers = getLayers(Map(lambda, data))
 
 	return tree
-
 }
+
+func hashFunction(data string) string {
+	return "hash(" + data + ")"
+}
+
+type Tree [][]string
+
+func Map(f func(string) string, xs []string) []string {
+	ys := make([]string, len(xs))
+	for i, x := range xs {
+		ys[i] = f(x)
+	}
+	return ys
+}
+
+func concat(a string, b string) string {
+	return "concat(" + a + "," + b + ")"
+}
+
+func getLayers(elements []string) Tree {
+	emptyLeveled := hashFunction("0")
+	if (len(elements) % 2) > 0 {
+		elements = append(elements, emptyLeveled)
+	}
+
+	myTree := Tree{elements}
+	const maxLevel = 31
+
+	for level := 1; level < 10; level++ {
+		var current []string
+		for i := 0; i < len(myTree[level-1])/2; i++ {
+			a := myTree[level-1][i*2]
+			b := myTree[level-1][i*2+1]
+			hash := hashFunction(concat(a, b))
+
+			current = append(current, hash)
+			// current.push(hash)
+		}
+
+		if (len(current)%2 > 0) && level < maxLevel {
+			//current.push(emptyLeveled);
+			current = append(current, emptyLeveled)
+		}
+
+		emptyLeveled = hashFunction(concat(emptyLeveled, emptyLeveled))
+
+		// tree.push(current);
+		myTree = append(myTree, current)
+	}
+
+	//for (let level = 1; level <= maxLevel; level++) {
+	//}
+
+	return myTree
+}
+
+func (tree *MerkleTree) getRoot(elements []string) string {
+	return tree.Layers[len(tree.Layers)-1][0]
+}
+
+//func getHexRoot () {
+//	return bufferToHex(this.getRoot());
+//}
+//
+//getProof (idx) {
+//	if (idx == = -1) {
+//		throw new Error('Element does not exist in Merkle tree');
+//	}
+//
+//	return this.layers.reduce((proof, layer) = > {
+//	const pairElement = this.getPairElement(idx, layer);
+//	if (pairElement) {
+//		proof.push(pairElement);
+//	}
+//
+//	idx = Math.floor(idx / 2);
+//	return proof;
+//	}, []);
+//}
+//
+//getHexProof (idx) {
+//	const result = this.getProof(idx);
+//	return this.bufArrToHexArr(result);
+//}
+//
+//getPairElement (idx, layer) {
+//	const pairIdx = idx % 2 == = 0 ? idx + 1: idx - 1;
+//	if (pairIdx < layer.length) {
+//		return layer[pairIdx];
+//	} else {
+//		return null;
+//	}
+//}
+//
+//bufIndexOf (el, arr) {
+//let hash;
+//
+//// Convert element to 32 byte hash if it is not one already
+//if (el.length != = 32 || !Buffer.isBuffer(el)) {
+//hash = this.hashFunction(el);
+//} else {
+//hash = el;
+//}
+//
+//for (let i = 0; i < arr.length; i++) {
+//if (hash.equals(arr[i])) {
+//return i;
+//}
+//}
+//
+//return -1;
+//}
+//
+//bufArrToHexArr (arr) {
+//if (arr.some(el = > !Buffer.isBuffer(el))) {
+//throw new Error('Array is not an array of buffers');
+//}
+//
+//return arr.map(el = > '0x' + el.toString('hex'));
+//}
+//}
+//
+//function keccak160 (element) {
+//return keccak256(element).slice(12,32);
+//}
+//
+//module.exports = {
+//MerkleTree,
+//keccak160
+//};
