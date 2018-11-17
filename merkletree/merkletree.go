b1f3b8e6e63e3bec7c5c5ba79900da139b9f08c5
Nikita Kozlov <erage@MacBook-Pro-Nikita.local>
2018-11-17 21:56:04 +0300

    starting rewriting all

--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -1,144 +1,78 @@
 package merkletree
 
 type MerkleTree struct {
-	Layers [][]string
+	nodes *[]Nodes
 }
 
-func NewMerkleTree(data []string) *MerkleTree {
-	tree := new(MerkleTree)
+type Node struct {
+	value []byte
+}
 
-	lambda := func(x string) string { return hashFunction(x) }
-	tree.Layers = getLayers(Map(lambda, data))
+type Nodes struct {
+	node *[]Node
+}
+
+type Params struct {
+	level *uint64
+}
+
+type Elements struct {
+	Elements []Element
+}
+
+type ElementMethodos interface {
+	Equal(el Element) (bool)
+	Hash() ([]byte)
+	Concat(empty Element, emptySecond Element) (Element)
+}
+
+type Element struct {
+	Element string
+
+	ElementMethodos
+}
+
+func Tree(params *Params, elements *Elements) *MerkleTree {
+
+	empty := new(Element)
+	empty.Element = "|"
+
+	nodes := new(Nodes)
+	nodes.node = makeNodes(elements, empty)
+
+	nodesGroup := make([]Nodes, 0)
+	nodesGroup = append(nodesGroup, *nodes)
+	nodesGroup = append(nodesGroup, *nodes)
+
+	tree := new(MerkleTree)
+	tree.nodes = &nodesGroup
 
 	return tree
 }
 
-func hashFunction(data string) string {
-	return "hash(" + data + ")"
-}
+func makeNodes(elements *Elements, empty *Element) *[]Node {
 
-type Tree [][]string
+	nodeGroup := make([]Node, 0)
 
-func Map(f func(string) string, xs []string) []string {
-	ys := make([]string, len(xs))
-	for i, x := range xs {
-		ys[i] = f(x)
-	}
-	return ys
-}
+	e := *elements
 
-func concat(a string, b string) string {
-	return "concat(" + a + "," + b + ")"
-}
-
-func getLayers(elements []string) Tree {
-	emptyLeveled := hashFunction("0")
-	if (len(elements) % 2) > 0 {
-		elements = append(elements, emptyLeveled)
+	for i := 0; i < len(e.Elements); i++ {
+		n := new(Node)
+		n.value = e.Elements[i].Hash()
+		nodeGroup = append(nodeGroup, *n)
 	}
 
-	myTree := Tree{elements}
-	const maxLevel = 31
-
-	for level := 1; level < 10; level++ {
-		var current []string
-		for i := 0; i < len(myTree[level-1])/2; i++ {
-			a := myTree[level-1][i*2]
-			b := myTree[level-1][i*2+1]
-			hash := hashFunction(concat(a, b))
-
-			current = append(current, hash)
-			// current.push(hash)
-		}
-
-		if (len(current)%2 > 0) && level < maxLevel {
-			//current.push(emptyLeveled);
-			current = append(current, emptyLeveled)
-		}
-
-		emptyLeveled = hashFunction(concat(emptyLeveled, emptyLeveled))
-
-		// tree.push(current);
-		myTree = append(myTree, current)
+	if len(nodeGroup)%2 != 0 {
+		n := new(Node)
+		emp := makeEmptyElement(*empty)
+		n.value = emp.Hash()
+		nodeGroup = append(nodeGroup, *n)
 	}
 
-	//for (let level = 1; level <= maxLevel; level++) {
-	//}
-
-	return myTree
+	return &nodeGroup
 }
 
-func (tree *MerkleTree) getRoot(elements []string) string {
-	return tree.Layers[len(tree.Layers)-1][0]
+func makeEmptyElement(empty Element) Element {
+	empty = empty.Concat(empty, empty)
+	return empty
 }
-
-//func getHexRoot () {
-//	return bufferToHex(this.getRoot());
-//}
-//
-//getProof (idx) {
-//	if (idx == = -1) {
-//		throw new Error('Element does not exist in Merkle tree');
-//	}
-//
-//	return this.layers.reduce((proof, layer) = > {
-//	const pairElement = this.getPairElement(idx, layer);
-//	if (pairElement) {
-//		proof.push(pairElement);
-//	}
-//
-//	idx = Math.floor(idx / 2);
-//	return proof;
-//	}, []);
-//}
-//
-//getHexProof (idx) {
-//	const result = this.getProof(idx);
-//	return this.bufArrToHexArr(result);
-//}
-//
-//getPairElement (idx, layer) {
-//	const pairIdx = idx % 2 == = 0 ? idx + 1: idx - 1;
-//	if (pairIdx < layer.length) {
-//		return layer[pairIdx];
-//	} else {
-//		return null;
-//	}
-//}
-//
-//bufIndexOf (el, arr) {
-//let hash;
-//
-//// Convert element to 32 byte hash if it is not one already
-//if (el.length != = 32 || !Buffer.isBuffer(el)) {
-//hash = this.hashFunction(el);
-//} else {
-//hash = el;
-//}
-//
-//for (let i = 0; i < arr.length; i++) {
-//if (hash.equals(arr[i])) {
-//return i;
-//}
-//}
-//
-//return -1;
-//}
-//
-//bufArrToHexArr (arr) {
-//if (arr.some(el = > !Buffer.isBuffer(el))) {
-//throw new Error('Array is not an array of buffers');
-//}
-//
-//return arr.map(el = > '0x' + el.toString('hex'));
-//}
-//}
-//
-//function keccak160 (element) {
-//return keccak256(element).slice(12,32);
-//}
-//
-//module.exports = {
-//MerkleTree,
-//keccak160
-//};
